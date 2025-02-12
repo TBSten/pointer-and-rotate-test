66f385a04057aaf25fc97bc44e542cd898beebae
TBSten <81161390+TBSten@users.noreply.github.com>
2025-02-12 22:09:00 +0900

    Add 2 Screens

--- app/src/main/kotlin/your/projectPackage/app/Case1_Expected.kt
+++ app/src/main/kotlin/your/projectPackage/app/Case1_Expected.kt
@@ -1,2 +1,7 @@
 package your.projectPackage.app
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ExpectedScreen() {
+}
--- app/src/main/kotlin/your/projectPackage/app/MainActivity.kt
+++ app/src/main/kotlin/your/projectPackage/app/MainActivity.kt
@@ -10,16 +10,20 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.internal.composableLambda
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.map
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -28,37 +32,47 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val navController = rememberNavController()
+            MaterialTheme {
+                val navController = rememberNavController()
+                val currentRoute by navController.currentBackStackEntryFlow
+                    .map { it.destination.route }
+                    .collectAsState(null)
 
-            Scaffold(
-                topBar = {
-                    TopAppBar(
-                        title = {
-                            Text(navController.currentDestination?.route ?: "")
-                        },
-                    )
-                },
-                bottomBar = {
-                    NavigationBar {
-                        listOf("expected", "unexpected").forEach { route ->
-                            NavigationBarItem(
-                                selected = route == navController.currentDestination?.route,
-                                icon = { Icon(Icons.Default.Home, contentDescription = null) },
-                                label = { Text(route) },
-                                onClick = {
-                                    navController.navigate(route)
-                                },
-                            )
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(currentRoute ?: "")
+                            },
+                        )
+                    },
+                    bottomBar = {
+                        NavigationBar {
+                            listOf("expected", "unexpected").forEach { route ->
+                                NavigationBarItem(
+                                    selected = route == currentRoute,
+                                    icon = { Icon(Icons.Default.Home, contentDescription = null) },
+                                    label = { Text(route) },
+                                    onClick = {
+                                        navController.navigate(route)
+                                    },
+                                )
+                            }
                         }
-                    }
-                },
-            ) { innerPadding ->
-                Column(Modifier.padding(innerPadding)) {
-                    NavHost(
-                        navController = navController,
-                        startDestination = "expected",
-                    ) {
-                        composableLambda {}
+                    },
+                ) { innerPadding ->
+                    Column(Modifier.padding(innerPadding)) {
+                        NavHost(
+                            navController = navController,
+                            startDestination = "expected",
+                        ) {
+                            composable("expected") {
+                                ExpectedScreen()
+                            }
+                            composable("unexpected") {
+                                UnexpectedScreen()
+                            }
+                        }
                     }
                 }
             }
